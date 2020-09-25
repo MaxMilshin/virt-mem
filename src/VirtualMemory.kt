7a6fc204e0986f6a3a698f3606faaab8ba9d2503
MaxMilshin <Maxonchik2003@yandex.ru>
2020-09-26 02:05:49 +0300

    Second commit(add file input)

--- src/VirtualMemory.kt
+++ src/VirtualMemory.kt
@@ -1,16 +1,18 @@
-fun input(pages: MutableList<Int>) : Pair<Int, Int> {
-    val n = readLine()!!.toInt()
-    val m = readLine()!!.toInt()
-    val numOfPages = readLine()!!.toInt()
-    for (i in 0 until numOfPages) {
-        pages.add(readLine()!!.toInt())
-    }
+import java.io.File
+
+fun input(fileName: String, pages: MutableList<Int>) : Pair<Int, Int> {
+    val lines: List<String> = File(fileName).readLines()
+    val n = lines[0].toInt()
+    val m = lines[1].toInt()
+    var sequenceOfPages = lines[2].split(" ").toTypedArray()
+    for (elem in sequenceOfPages)
+        pages.add(elem.toInt())
     return Pair(n, m)
 }
 
-fun main() {
+fun main(args: Array<String>) {
     var pages = mutableListOf<Int>()
-    val res = input(pages)
+    val res = input(args[0], pages)
     val n = res.first
     val m = res.second
     var memory = IntArray(m) {-1}
