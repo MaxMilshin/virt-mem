2cf6d86b11e31684382d75f1dd01356698e46667
MaxMilshin <Maxonchik2003@yandex.ru>
2020-10-25 21:07:42 +0300

    fix testing script one more time

--- integrationTesting.sh
+++ integrationTesting.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
+result=0
 kotlinc src/CommonAlgorithm.kt src/InputParser.kt src/ReplacementPosition.kt src/VirtMem.kt src/VirtualMemory.kt -include-runtime -d virtMem.jar
 for ((i = 1 ; i <= 10 ; i++)); do
   java -jar virtMem.jar data/test${i}.in > output.txt
   if (diff expectedAnswers/out${i}.in output.txt); then
-    echo "PASSED"
+    echo "$i: passed"
   else
-    echo "FAILED"
+    echo "$i: failed"
+    result=1
   fi;
   truncate -s 0 output.txt
 done
+exit $result
 
