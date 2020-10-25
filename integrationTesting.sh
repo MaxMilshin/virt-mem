5c224c6780ff3573216252a0a6569cd0d554b03a
MaxMilshin <Maxonchik2003@yandex.ru>
2020-10-25 20:02:13 +0300

    fix testing script

--- integrationTesting.sh
+++ integrationTesting.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+kotlinc src/CommonAlgorithm.kt src/InputParser.kt src/ReplacementPosition.kt src/VirtMem.kt src/VirtualMemory.kt -include-runtime -d virtMem.jar
 for ((i = 1 ; i <= 10 ; i++)); do
   java -jar virtMem.jar data/test${i}.in > output.txt
   if (diff expectedAnswers/out${i}.in output.txt); then
